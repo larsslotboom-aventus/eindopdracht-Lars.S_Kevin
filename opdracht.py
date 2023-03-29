a190ee52f637180c75eadafeeefab80348e98789
larsslotboom <larsslotboom52@gmail.com>
2023-03-29 13:55:34 +0200

    file open test

--- opdracht.py
+++ opdracht.py
@@ -1,14 +1,25 @@
 import tkinter as tk
+import os
+
+current_file_path = os.path.realpath(__file__)
+os.chdir(os.path.dirname(current_file_path))
+
 
 root = tk.Tk()
+root.title("main menu")
 
-button1 = tk.Button(root, text="Button 1")
-button2 = tk.Button(root, text="Button 2")
-button3 = tk.Button(root, text="Button 3")
-button4 = tk.Button(root, text="Button 4", height=6)
+def open_window(winID):
+    open("\logica\btw.py").read()
 
-button1.grid(row=0, column=0, padx=5, pady=5)
-button2.grid(row=1, column=0, padx=5, pady=5)
-button3.grid(row=2, column=0, padx=5, pady=5)
-button4.grid(row=0, column=1, rowspan=3, padx=5, pady=5)
+label = tk.Label(root, text="main menu", font=('arial',20))
+button1 = tk.Button(root, text="BTW Berekening", width=18, command=open_window(0))
+button2 = tk.Button(root, text="Tijdberekening", width=18)
+button3 = tk.Button(root, text="Vierkant Berekeningen", width=18)
+button4 = tk.Button(root, text="circel Berekeningen", width=18)
+
+label.grid(row=0, column=0, columnspan=4)
+button1.grid(row=1, column=0, padx=5, pady=5)
+button2.grid(row=1, column=1, padx=5, pady=5)
+button3.grid(row=1, column=2, padx=5, pady=5)
+button4.grid(row=1, column=3, padx=5, pady=5)
 root.mainloop()
