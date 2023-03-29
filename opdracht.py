5f0d062ba0b96b1f08a6d835de239c96b76bf6dd
larsslotboom <larsslotboom52@gmail.com>
2023-03-29 14:35:11 +0200

    tijdsberekening toegevoegd aan main bestand

--- opdracht.py
+++ opdracht.py
@@ -9,12 +9,38 @@ os.chdir(newdir)
 root = tk.Tk()
 root.title("main menu")
 
-def open_window():
-    newwindow= tk.Toplevel(root)
+def open_window_tijdberekening():
+    def calculate():
+        distance = float(distance_entry.get())
+        speed = float(speed_entry.get())
+        time = distance / speed
+        time_label.config(text="Reistijd: {:.2f} uur".format(time))
+
+    tijdberekening = tk.Toplevel(root)
+    tijdberekening.title("Reistijd berekenen")
+
+    distance_label = tk.Label(tijdberekening, text="Afstand (km):")
+    distance_label.grid(row=0, column=0, padx=5, pady=5)
+
+    distance_entry = tk.Entry(tijdberekening)
+    distance_entry.grid(row=0, column=1, padx=5, pady=5)
+
+    speed_label = tk.Label(tijdberekening, text="Snelheid (km/uur):")
+    speed_label.grid(row=1, column=0, padx=5, pady=5)
+
+    speed_entry = tk.Entry(tijdberekening)
+    speed_entry.grid(row=1, column=1, padx=5, pady=5)
+
+    calculate_button = tk.Button(tijdberekening, text="Bereken", command=calculate)
+    calculate_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+
+    time_label = tk.Label(tijdberekening, text="Reistijd: ")
+    time_label.grid(row=3, column=0, padx=5, pady=5)
+
 
 label = tk.Label(root, text="main menu", font=('arial',20))
-button1 = tk.Button(root, text="BTW Berekening", width=18, command=open_window)
-button2 = tk.Button(root, text="Tijdberekening", width=18)
+button1 = tk.Button(root, text="BTW Berekening", width=18)
+button2 = tk.Button(root, text="Tijdberekening", width=18, command=open_window_tijdberekening)
 button3 = tk.Button(root, text="Vierkant Berekeningen", width=18)
 button4 = tk.Button(root, text="circel Berekeningen", width=18)
 
