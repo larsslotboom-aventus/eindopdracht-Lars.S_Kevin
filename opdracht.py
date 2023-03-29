6e001b307257e2803a52cddae3edd06ca4b0d09b
larsslotboom <larsslotboom52@gmail.com>
2023-03-29 14:23:36 +0200

    got a better way of having multiple windows

--- opdracht.py
+++ opdracht.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.ttk import *
 import os
 
 newdir = __file__.replace('\\' + os.path.basename(__file__), '')
@@ -8,11 +9,11 @@ os.chdir(newdir)
 root = tk.Tk()
 root.title("main menu")
 
-def open_window(winID):
-    open("logica\btw.py").read()
+def open_window():
+    newwindow= tk.Toplevel(root)
 
 label = tk.Label(root, text="main menu", font=('arial',20))
-button1 = tk.Button(root, text="BTW Berekening", width=18, command=open_window(0))
+button1 = tk.Button(root, text="BTW Berekening", width=18, command=open_window)
 button2 = tk.Button(root, text="Tijdberekening", width=18)
 button3 = tk.Button(root, text="Vierkant Berekeningen", width=18)
 button4 = tk.Button(root, text="circel Berekeningen", width=18)
@@ -22,4 +23,5 @@ button1.grid(row=1, column=0, padx=5, pady=5)
 button2.grid(row=1, column=1, padx=5, pady=5)
 button3.grid(row=1, column=2, padx=5, pady=5)
 button4.grid(row=1, column=3, padx=5, pady=5)
+
 root.mainloop()
