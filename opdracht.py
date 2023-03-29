6516620ef94eb9d2a7b88e981656bf4d2b6f2d63
larsslotboom <larsslotboom52@gmail.com>
2023-03-29 14:43:00 +0200

    alles gelinked behalve BTW

--- opdracht.py
+++ opdracht.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter.ttk import *
+from math import pi
 import os
 
 newdir = __file__.replace('\\' + os.path.basename(__file__), '')
@@ -8,6 +9,62 @@ os.chdir(newdir)
 
 root = tk.Tk()
 root.title("main menu")
+
+def open_window_vierkant():
+    def calculate():
+        length = float(length_entry.get())
+        width = float(width_entry.get())
+        perimeter = 2 * (length + width)
+        area = length * width
+        perimeter_label.config(text="Omtrek: {:.2f}".format(perimeter))
+        area_label.config(text="Oppervlakte: {:.2f}".format(area))
+    
+    vierkant = tk.Toplevel(root)
+    vierkant.title("Vierkant berekeningen")
+    
+    length_label = tk.Label(vierkant, text="Lengte:")
+    length_label.grid(row=0, column=0, padx=5, pady=5)
+    
+    length_entry = tk.Entry(vierkant)
+    length_entry.grid(row=0, column=1, padx=5, pady=5)
+    
+    width_label = tk.Label(vierkant, text="Breedte:")
+    width_label.grid(row=1, column=0, padx=5, pady=5)
+    
+    width_entry = tk.Entry(vierkant)
+    width_entry.grid(row=1, column=1, padx=5, pady=5)
+    
+    calculate_button = tk.Button(vierkant, text="Bereken", command=calculate)
+    calculate_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+    
+    perimeter_label = tk.Label(vierkant, text="Omtrek: ")
+    perimeter_label.grid(row=3, column=0, padx=5, pady=5)
+    
+    area_label = tk.Label(vierkant, text="Oppervlakte: ")
+    area_label.grid(row=4, column=0, padx=5, pady=5)
+    
+
+def open_window_cirkel():
+    def calculate():
+        diameter = float(entry.get())
+        circumference = pi * diameter
+        area = pi * (diameter/2)**2
+        result_label.config(text=f"Omtrek: {circumference:.2f}\nOppervlakte: {area:.2f}")
+
+    cirkel = tk.Toplevel(root)
+    cirkel.title("Bereken omtrek en oppervlakte van een cirkel")
+
+    label = tk.Label(cirkel, text="Voer de diameter in:")
+    label.pack()
+
+    entry = tk.Entry(cirkel)
+    entry.pack()
+
+    button = tk.Button(cirkel, text="Bereken", command=calculate)
+    button.pack()
+
+    result_label = tk.Label(cirkel, text="")
+    result_label.pack()
 
 def open_window_tijdberekening():
     def calculate():
@@ -41,8 +98,8 @@ def open_window_tijdberekening():
 label = tk.Label(root, text="main menu", font=('arial',20))
 button1 = tk.Button(root, text="BTW Berekening", width=18)
 button2 = tk.Button(root, text="Tijdberekening", width=18, command=open_window_tijdberekening)
-button3 = tk.Button(root, text="Vierkant Berekeningen", width=18)
-button4 = tk.Button(root, text="circel Berekeningen", width=18)
+button3 = tk.Button(root, text="Vierkant Berekeningen", width=18, command=open_window_vierkant)
+button4 = tk.Button(root, text="circel Berekeningen", width=18, command=open_window_cirkel)
 
 label.grid(row=0, column=0, columnspan=4)
 button1.grid(row=1, column=0, padx=5, pady=5)
